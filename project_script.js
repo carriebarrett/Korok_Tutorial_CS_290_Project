ff99faba6abf95a2c8169aa8f769888bd361b83b
Carrie Barrett <67287344+carriebarrett@users.noreply.github.com>
2021-02-20 19:40:51 -0800

    started slideshow

--- project_script.js
+++ project_script.js
@@ -22,6 +22,30 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
   res.render('home');
+  
+  // slideshow is similar to https://www.w3schools.com/w3css/w3css_slideshow.asp
+  var slideshowIndex = 0;
+  showImg(slideshowIndex);
+  
+  function incrSlideshow(change) {
+    showImg(slideshowIndex += change);// shows the image with the next index
+  }
+  
+  function showImg(index) {
+    var i;
+    var slideList = document.getElementsByClassName("mySlides");
+    if (n > slideList.length) {
+	  slideshowIndex = 1;
+	}
+    if (n < 0) {
+	  slideIndex = slideList.length - 1;
+	}
+    for (i = 0; i < slideList.length; i++) {
+      slideList[i].style.display = "none";
+    }
+    slideList[slideshowIndex].style.display = "block";
+  }
+  
 });
 
 app.get('/fruit_trees', function(req, res) {
